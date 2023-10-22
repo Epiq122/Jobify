3c7472b94607b1f9d32a47ebdffb193e430cf62d
Robert Gleason <l2ob122@gmail.com>
2023-10-21 20:59:27 -0700

    added styling to the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,70 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../components/Logo.jsx";
+import FormRow from "../components/FormRow.jsx";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to={'/register'}>Register</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Login</h4>
+        <FormRow type="email" name="email" defaultValue="rob@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="test123" />
+        <button type="button" className="btn btn-block">
+          submit
+        </button>
+        <button type="button" className="btn btn-block">
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+      <Link to={"/register"}>Register</Link>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h4 {
+    text-align: center;
+    margin-bottom: 1.38rem;
+  }
+
+  p {
+    margin-top: 1rem;
+    text-align: center;
+    line-height: 1.5;
+  }
+
+  .btn {
+    margin-top: 1rem;
+  }
+
+  .member-btn {
+    color: var(--primary-500);
+    letter-spacing: var(--letter-spacing);
+    margin-left: 0.25rem;
+  }
+`;
+
 export default Login;
