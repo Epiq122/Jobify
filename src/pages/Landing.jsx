0a6e2546c98b45ba389eb48d5510ec29679c8cfd
Robert Gleason <l2ob122@gmail.com>
2023-10-21 19:55:34 -0700

    imported styled components into the landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import styled from "styled-components";
+
+const StyledBtn = styled.button`
+  font-size: 1.5rem;
+  background: red;
+  color: white;
+`;
 
 const Landing = () => {
-    return (
-        <div>Landing</div>
-    )
-}
-export default Landing
+  return (
+    <div>
+      <h1>Landing page</h1>
+      <StyledBtn>Click me</StyledBtn>
+    </div>
+  );
+};
+export default Landing;
