955d74fb366043ddf01f67cdfa900f38655c63f9
Robert Gleason <l2ob122@gmail.com>
2023-10-21 21:15:40 -0700

    added side bar navbar component to the component, added the styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+const Navbar = () => {
+  return <Wrapper>Navbar</Wrapper>;
+};
 
 const Wrapper = styled.nav`
   height: var(--nav-height);
@@ -7,12 +11,14 @@ const Wrapper = styled.nav`
   justify-content: center;
   box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.1);
   background: var(--background-secondary-color);
+
   .nav-center {
     display: flex;
     width: 90vw;
     align-items: center;
     justify-content: space-between;
   }
+
   .toggle-btn {
     background: transparent;
     border-color: transparent;
@@ -22,30 +28,36 @@ const Wrapper = styled.nav`
     display: flex;
     align-items: center;
   }
+
   .logo-text {
     display: none;
   }
+
   .logo {
     display: flex;
     align-items: center;
     width: 100px;
   }
+
   .btn-container {
     display: flex;
     align-items: center;
   }
+
   @media (min-width: 992px) {
     position: sticky;
     top: 0;
     .nav-center {
       width: 90%;
     }
+
     .logo {
       display: none;
     }
+
     .logo-text {
       display: block;
     }
   }
 `;
-export default Wrapper;
+export default Navbar;
--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,10 +1,47 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import SmallSidebar from "../components/SmallSidebar.jsx";
+import BigSidebar from "../components/BigSidebar.jsx";
+import Navbar from "../components/Navbar.jsx";
 
 const DashboardLayout = () => {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.section`
+  .dashboard {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+
+  .dashboard-page {
+    width: 90vw;
+    margin: 0 auto;
+    padding: 2rem 0;
+  }
+
+  @media (min-width: 992px) {
+    .dashboard {
+      grid-template-columns: auto 1fr;
+    }
+
+    .dashboard-page {
+      width: 90%;
+    }
+  }
+`;
+
 export default DashboardLayout;
