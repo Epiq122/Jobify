ff5737e1a00076aa75d404522abcf1214e8c61b6
Robert Gleason <l2ob122@gmail.com>
2023-10-21 20:40:20 -0700

    added styling for register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,77 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../components/Logo.jsx";
+
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to={'/login'}>Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-input"
+            defaultValue="rob"
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+
+        <p>
+          Already a member?
+          <Link to={"/login"} className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h4 {
+    text-align: center;
+    margin-bottom: 1.38rem;
+  }
+
+  p {
+    margin-top: 1rem;
+    text-align: center;
+    line-height: 1.5;
+  }
+
+  .btn {
+    margin-top: 1rem;
+  }
+
+  .member-btn {
+    color: var(--primary-500);
+    letter-spacing: var(--letter-spacing);
+    margin-left: 0.25rem;
+  }
+`;
 
 export default Register;
