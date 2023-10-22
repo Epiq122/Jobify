aeb66c82b517cef9ab13a30e60ba4bab1fd89685
Robert Gleason <l2ob122@gmail.com>
2023-10-21 18:17:40 -0700

    added link to the register and login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    return (
-        <div>Login</div>
-    )
-}
-export default Login
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <Link to={'/register'}>Register</Link>
+    </div>
+  );
+};
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-
+import { Link } from 'react-router-dom';
 const Register = () => {
-    return (
-        <div>Register</div>
-    )
-}
-export default Register
+  return (
+    <div>
+      <h1>Register</h1>
+      <Link to={'/login'}>Login Page</Link>
+    </div>
+  );
+};
+
+export default Register;
